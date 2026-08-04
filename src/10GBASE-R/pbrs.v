507d0519cfac26ee09e0c615fc94c2ed04dbd73a
Julia Desmazes <julia.desmazes@gmail.com>
2026-08-04 11:36:40 -0500

    add pbrs7

--- src/10GBASE-R/pbrs.v
+++ src/10GBASE-R/pbrs.v
@@ -1,14 +1,19 @@
 
 /* 
-PBRS31 for link training 
-G(x) = 1 + x^{28} + x^{31} 
+PBRS31 and PRBS7 for link training 
+
+PRBS31: G(x) = 1 + x^{28} + x^{31} 
+
+PRBS7: G(x) = 1 + x^{6} + x^{7}
+
 */
 module pbrs31(
 	input wire clk, 
-
 	input wire        enable_i,	
 	input wire        rst_seed_i, 
 	input wire [31:0] seed_i,
+
+	input wire         prbs31_sel_i,
 
 	output wire [31:0] pbrs_o
 );
@@ -18,7 +23,8 @@ reg  [31:0]	buff = 0;
 
 always @(*) begin
 	for(integer i=31; i>=0; i--) begin
-		lfst_next[i] = lfsr_q[i] ^ buff[27] ^ buff[30];
+		lfst_next[i] = prbs31_sel_i? lfsr_q[i] ^ buff[27] ^ buff[30]: 
+									 lfsr_q[i] ^ buff[5] ^ buff[7];
 		buff = { buff[30:0], lfsr_q[i] };
 	end
 end
