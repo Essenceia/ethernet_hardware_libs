d12dd76220c6a2a4edd8aa9f1992984923527f4d
Julia Desmazes - ws <julia.desmazes@gmail.com>
2026-08-04 12:24:52 -0500

    save lint fix

--- src/10GBASE-R/pbrs.v
+++ src/10GBASE-R/pbrs.v
@@ -7,7 +7,7 @@ PRBS31: G(x) = 1 + x^{28} + x^{31}
 PRBS7: G(x) = 1 + x^{6} + x^{7}
 
 */
-module pbrs31(
+module pbrs(
 	input wire clk, 
 	input wire        enable_i,	
 	input wire        rst_seed_i, 
@@ -17,13 +17,13 @@ module pbrs31(
 
 	output wire [31:0] pbrs_o
 );
-wire [31:0] lfsr_next; 
-reg  [31:0] lfsr_q; 
-reg  [31:0]	buff = 0;
+reg [31:0] lfsr_next; 
+reg [31:0] lfsr_q; 
+reg [31:0] buff = 0;
 
 always @(*) begin
 	for(integer i=31; i>=0; i--) begin
-		lfst_next[i] = prbs31_sel_i? lfsr_q[i] ^ buff[27] ^ buff[30]: 
+		lfsr_next[i] = prbs31_sel_i? lfsr_q[i] ^ buff[27] ^ buff[30]: 
 									 lfsr_q[i] ^ buff[5] ^ buff[7];
 		buff = { buff[30:0], lfsr_q[i] };
 	end
